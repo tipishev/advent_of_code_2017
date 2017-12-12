353a1986fac3af3fb4b58e5789102a85b2d16bef
Timofey Tipishev <tipishev@gmail.com>
2017-12-12 23:35:13 +0100

    apologize to Dijkstra with a better algorithm

--- 12.py
+++ 12.py
@@ -1,3 +1,6 @@
+INFINITY = 999999
+
+
 def parse(filename):
     result = dict()
     with open(filename, 'r') as f:
@@ -8,20 +11,32 @@ def parse(filename):
     return result
 
 
-#  data = parse('12_input_small.txt')
-data = parse('12_input.txt')
+def Dijkstra(graph, initial):
+    distances = dict.fromkeys(graph, INFINITY)
 
-zero_group = {0}
-for program, peers in data.items():
-    combined_set = {program}.union(peers)
-    intersection = zero_group.intersection(combined_set)
-    if intersection:
-        zero_group = zero_group.union(combined_set)
+    distances[initial] = 0
+    unvisited = set(graph)
 
-for _ in range(1000):
-    for key in list(zero_group):
-        zero_group = zero_group.union(data[key])
+    while unvisited:
+        items = [(k, v) for (k, v) in distances.items() if k in unvisited]
+        current_node, current_node_distance = min(items,
+                                                  key=lambda item: item[1])
+        unvisited.remove(current_node)
+
+        for neighbor in graph[current_node]:
+            current_neighbor_distance = distances[neighbor]
+            distances[neighbor] = min(current_neighbor_distance,
+                                      current_node_distance + 1)
+
+    return distances
 
 
-print(sorted(zero_group))
-print(len(zero_group))
+def count_reachable(graph, initial):
+    distances = Dijkstra(graph, initial)
+    return len([(k, v) for (k, v) in distances.items() if v != INFINITY])
+
+
+#  graph = parse('12_input_small.txt')
+graph = parse('12_input.txt')
+
+print(count_reachable(graph, 0))
